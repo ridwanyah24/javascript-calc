85b1284dae528752a22325a690f0d0033f496435
ridwanyah23 <ridwangames24@gmail.com>
2024-12-11 22:58:46 +0100

    latest update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+function Calc(){
+  var [answer, setAnswer] = useState(0);
+
+  setAnswer(answer= 35)
+  return (<>
+        <button value={1}></button>
+        <button value={2}></button>
+        <div className='display'></div>
+        <div>{answer}</div>
+          </>)
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calc />
   </React.StrictMode>
 );
 
